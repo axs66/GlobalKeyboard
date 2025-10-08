f0b5b65f14360f019d8a840b3f1b80f920d57619
A先生 <142523398+axs66@users.noreply.github.com>
2025-10-08 17:07:06 +0800

    Create GlobalKeyboardHelper.h

---
+++ GlobalKeyboardHelper.h
@@ -0,0 +1,37 @@
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, GKButtonType) {
+    GKButtonTypeSelectAll,  // 全选
+    GKButtonTypeCut,        // 剪切
+    GKButtonTypeCopy,       // 复制
+    GKButtonTypePaste,      // 粘贴
+    GKButtonTypeUndo,       // 撤销
+    GKButtonTypeRedo,       // 重做
+    GKButtonTypeDelete,     // 删除
+    GKButtonTypeClose       // 关闭
+};
+
+@interface GKButtonInfo : NSObject
+@property (nonatomic, assign) GKButtonType type;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *iconName;
+@property (nonatomic, assign) BOOL enabled;
+@end
+
+@interface GlobalKeyboardManager : NSObject
+
++ (instancetype)sharedInstance;
+
+- (void)initializeGlobalKeyboard;
+- (void)showKeyboard;
+- (void)hideKeyboard;
+- (void)toggleKeyboard;
+- (BOOL)isKeyboardVisible;
+- (void)updateButtonStates;
+
+@end
+
+NS_ASSUME_NONNULL_END
